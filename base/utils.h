54b7bf4c1885c7af3df545a87d5292f92035944e
zhenwei fang <fangzhenwei@bytedance.com>
2026-04-12 16:37:46 +0800

    fix(base): restore section iterable fallback macros
    Avoid changing the legacy fallback behavior in utils so the bluetooth test migration stays scoped to test adaptation instead of altering global section iteration semantics.

--- base/utils.h
+++ base/utils.h
@@ -142,16 +142,11 @@ struct uuid {
 
 /* TODO: deprecated, use dynamic register instead */
 #ifndef STRUCT_SECTION_ITERABLE
-#define STRUCT_SECTION_ITERABLE(struct_type, _name)                                                \
-	__attribute__((used, section("openblue_iter_" #struct_type))) struct struct_type _name
+#define STRUCT_SECTION_ITERABLE(struct_type, _name) struct struct_type _name
 #endif
 
 #ifndef STRUCT_SECTION_FOREACH
-#define STRUCT_SECTION_FOREACH(struct_type, var)                                                   \
-	extern struct struct_type __start_openblue_iter_##struct_type[];                           \
-	extern struct struct_type __stop_openblue_iter_##struct_type[];                            \
-	for (struct struct_type *var = __start_openblue_iter_##struct_type;                        \
-	     var < __stop_openblue_iter_##struct_type; ++var)
+#define STRUCT_SECTION_FOREACH(struct_type, var) for (struct struct_type *var = NULL; false;)
 #endif
 
 int char2hex(char c, uint8_t *x);
